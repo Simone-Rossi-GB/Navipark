67c6c2cb6b86b418441c0e13c2842b33915f0de3
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-02-25 14:41:03 +0100

    Auto-commit: modifiche del 2026-02-25 14:41

--- php_back-end_sviluppo/Util/StatusCode.php
+++ php_back-end_sviluppo/Util/StatusCode.php
@@ -1,10 +1,22 @@
 <?php
 class StatusCode
 {
+    public const AUTH_TOKEN_NOT_FOUND = [
+        'httpStatus' => 400,
+        'errorCode' => 'A001',
+        'message' => 'Token di sessione non presente nella richiesta'
+    ];
+
     public const AUTH_TOKEN_NOT_VALID = [
         'httpStatus' => 401,
-        'errorCode' => 'A001',
+        'errorCode' => 'A003',
         'message' => 'Token di sessione non valido'
+    ];
+
+    public const AUTH_TOKEN_NOT_EXPIRED = [
+        'httpStatus' => 401,
+        'errorCode' => 'A003',
+        'message' => 'Token di sessione scaduto. Rifare il login'
     ];
 
     public const DB_CONNECTION_FAILED = [
