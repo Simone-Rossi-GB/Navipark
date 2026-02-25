64092a136522f6b497110775d2001574e0f021e1
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-02-25 11:18:07 +0100

    Auto-commit: modifiche del 2026-02-25 11:18

---
+++ php_back-end_sviluppo/Middleware/AuthMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Middleware;
+
+class AuthMiddleware
+{
+    public static function authenticateToken() {
+
+    }
+
+    public static function login() {
+
+    }
+
+    public static function register() {
+
+    }
+
+    public static function generateToken() {
+        
+    }
+}
