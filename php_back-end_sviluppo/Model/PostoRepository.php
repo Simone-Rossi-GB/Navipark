a2d5466ffb9b6c224ecc704511ca73e7a2a9aae2
Marco Freddi <marcofreddi07@gmail.com>
2026-02-27 08:45:19 +0100

    Implementati Repository principali

---
+++ php_back-end_sviluppo/Model/PostoRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Model;
+use Util\Connection;
+
+class TennistaRepository{
+
+    private $config;
+
+    public function __construct($config){
+        $this->config = $config;
+    }
+
+    public function getPostoById(string $id) : array{
+        $pdo = Connection::getInstance();
+        $stmt = $pdo->prepare('SELECT * FROM posti WHERE id = :id');
+        $stmt->execute([
+            'id' => $id
+        ]);
+        return $stmt->fetch();
+    }
+
+    public function getPostoByParcheggioId(string $parcheggio_id) : array{
+        $pdo = Connection::getInstance();
+        $stmt = $pdo->prepare('SELECT * FROM posti WHERE parcheggio_id = :parcheggio_id');
+        $stmt->execute([
+            'parcheggio_id' => $parcheggio_id
+        ]);
+        return $stmt->fetch();
+    }
+}
---
+++ php_back-end_sviluppo/Model/PrenotazioneRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Model;
+use Util\Connection;
+
+class TennistaRepository{
+
+    private $config;
+
+    public function __construct($config){
+        $this->config = $config;
+    }
+
+    public function getPrenotazioneById(string $id) : array{
+        $pdo = Connection::getInstance();
+        $stmt = $pdo->prepare('SELECT * FROM prenotazioni WHERE id = :id');
+        $stmt->execute([
+            'id' => $id
+        ]);
+        return $stmt->fetch();
+    }
+
+    public function getPrenotazioneByDataOraInizio(string $data_ora_inizio) : array{
+        $pdo = Connection::getInstance();
+        $stmt = $pdo->prepare('SELECT * FROM prenotazioni WHERE data_ora_inizio = :data_ora_inizio');
+        $stmt->execute([
+            'data_ora_inizio' => $data_ora_inizio
+        ]);
+        return $stmt->fetch();
+    }
+
+    public function getPrenotazioneByDataOraFine(string $data_ora_fine) : array{
+        $pdo = Connection::getInstance();
+        $stmt = $pdo->prepare('SELECT * FROM prenotazioni WHERE data_ora_fine = :data_ora_fine');
+        $stmt->execute([
+            'data_ora_fine' => $data_ora_fine
+        ]);
+        return $stmt->fetch();
+    }
+
+    public function getPrenotazioneOccupyingDataInterval(string $data_ora_inizio, string $data_ora_fine) : array{
+        $pdo = Connection::getInstance();
+        $stmt = $pdo->prepare('SELECT * FROM prenotazioni WHERE data_ora_inizio BETWEEN :data_ora_inizio AND :data_ora_fine OR data_ora_fine BETWEEN :data_ora_inizio AND :data_ora_fine');
+        $stmt->execute([
+            'data_ora_inizio' => $data_ora_inizio,
+            'data_ora_fine' => $data_ora_fine
+        ]);
+        return $stmt->fetch();
+    }
+}
