e9ce25c31692b137b3283b70a15c025d207e82a7
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-03-12 13:28:55 +0100

    FIX 01: risolto bug per il quale premendo un marker sulla mappa apriva il modale invece del popup preview del parcheggio.
    INFO: aggiunto SearchBookingCodeEngine.jsx per la pagina di ricerca di prenotazioni tramite codice prenotazione.

---
+++ front-end/src/features/SearchBookingCodeEngine.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function SearchBookingCodeEngine() {
+    const [bookingCode, setBookingCode] = useState('');
+
+    return (
+        <div>
+
+        </div>
+    )
+}
