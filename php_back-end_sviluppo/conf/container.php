598eacd8bbb3047b3b0349ddb96bc2e6cdf63369
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2026-03-27 00:05:43 +0100

    creato l'.htaccess, Modificato il composer.json, eliminati file php sul posto parcheggio inutile, inseriti gli import nel container.php, creati ulteriori file di util.

---
+++ php_back-end_sviluppo/conf/container.php
@@ -0,0 +1,11 @@
+<?php
+use DI\ContainerBuilder;
+use Util\Connection;
+use Model\UtenteRepository;
+use Model\ParcheggioRepository;
+use Model\PrenotazioneRepository;
+use Model\SessioneRepository;
+use Controller\AuthController;
+use Controller\UtenteController;
+use Controller\ParcheggioController;
+use Controller\PrenotazioneController;
