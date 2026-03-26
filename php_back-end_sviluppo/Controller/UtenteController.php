c71678e9fb663d8cd347aa3d13862a5c366766fb
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2026-03-27 00:16:34 +0100

    creata la classe utentecontroller di base, finito il container con tutte le istanze necessarie, cambiato il nome di una variabile nel config.php, finito il middleware.php

--- php_back-end_sviluppo/Controller/UtenteController.php
+++ php_back-end_sviluppo/Controller/UtenteController.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace Controller;
+
+use Psr\Container\ContainerInterface;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+require_once 'Model/ParcheggioRepository.php';
+use Model\ParcheggioRepository;
+
+class UtenteController {
+    private $container;
+
+    // constructor receives container instance
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+}
