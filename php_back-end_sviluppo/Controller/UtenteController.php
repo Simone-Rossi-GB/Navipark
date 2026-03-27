50c529ba580167e5a1500a9d06852d72022542bf
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2026-03-27 01:32:12 +0100

    scritti i file controller

--- php_back-end_sviluppo/Controller/UtenteController.php
+++ php_back-end_sviluppo/Controller/UtenteController.php
@@ -1,20 +1,33 @@
 <?php
-
 namespace Controller;
 
-use Psr\Container\ContainerInterface;
-use Slim\Psr7\Request;
-use Slim\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Model\UtenteRepository;
+use Util\StatusCode;
+use Util\JsonResponse;
+use Util\Validator;
 
-require_once 'Model/ParcheggioRepository.php';
-use Model\ParcheggioRepository;
+class UtenteController
+{
+    public function __construct(private UtenteRepository $utenti) {}
 
-class UtenteController {
-    private $container;
-
-    // constructor receives container instance
-    public function __construct(ContainerInterface $container)
+    public function update(Request $request, Response $response, array $args): Response
     {
-        $this->container = $container;
+        $me = $request->getAttribute('utente');
+
+        // Un utente può modificare solo se stesso (admin può modificare chiunque)
+        if ($args['id'] !== $me['utente_id'] && $me['ruolo'] !== 'Admin') {
+            return JsonResponse::error($response, StatusCode::AUTH_ACCESSO_NEGATO);
+        }
+
+        $body     = $request->getParsedBody() ?? [];
+        $mancanti = Validator::required($body, ['nome', 'cognome', 'telefono']);
+        if (!empty($mancanti)) {
+            return JsonResponse::error($response, StatusCode::VALIDAZIONE_CAMPI_MANCANTI, ['campi' => array_values($mancanti)]);
+        }
+
+        $this->utenti->update($args['id'], $body);
+        return JsonResponse::success($response, StatusCode::UTENTE_AGGIORNATO, $this->utenti->findById($args['id']));
     }
 }
