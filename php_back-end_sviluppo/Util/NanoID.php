7906aed77f6998c53c5efe2f4e85f71082e40105
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2026-03-27 01:28:08 +0100

    terminato NanoID (testare la libreria) e finito UtenteRepository.php

--- php_back-end_sviluppo/Util/NanoID.php
+++ php_back-end_sviluppo/Util/NanoID.php
@@ -1,1 +1,16 @@
 <?php
+
+use Hidehalo\Nanoid\Client;
+use Hidehalo\Nanoid\GeneratorInterface;
+
+namespace Util;
+
+class NanoID {
+
+    public static function generate(): string {
+        $client = new Client();
+        $result = $client->generateId($size = 21);
+
+        return $result;
+    }
+}
