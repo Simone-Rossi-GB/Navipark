a373813c8f27b4880fd0f533dc9955187e7d71ba
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2026-03-27 01:05:55 +0100

    terminato il JwtHelper.php con le due funzioni generate e verify.

--- php_back-end_sviluppo/Util/JwtHelper.php
+++ php_back-end_sviluppo/Util/JwtHelper.php
@@ -1,1 +1,33 @@
 <?php
+namespace Util;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\ExpiredException;
+
+// non toccare questa classe che la sto controllando
+
+class JwtHelper {
+
+    // Genera il JWT token
+    public static function generate(string $utenteId, string $secret, int $ttl): string {
+        $payload = [
+            'iss' => 'Navipark-Brescia', // l'ente che rilascia il token
+            'sub' => $utenteId, // a chi è associato
+            'iat' => time(), // orario generazione
+            'exp' => time() + $ttl // scadenza token
+        ];
+
+        return JWT::encode($payload, $secret, 'H256');
+    }
+
+    // Ritorna l'utente_id (sub) se il token è valido, null altrimenti
+    public static function verify(string $token, string $secret): ?string {
+        try {
+            $decoded = JWT::decode($token, new Key($secret, 'H256'));
+            return $decoded->sub;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
