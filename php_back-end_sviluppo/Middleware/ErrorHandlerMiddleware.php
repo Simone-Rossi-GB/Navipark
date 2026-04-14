ff6037d4e65ba9dbcf2a0226ef67a1d13737e0fd
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-04-14 12:57:04 +0200

    13/04/26: container di monitoraggio completati. ultime modifiche

--- php_back-end_sviluppo/Middleware/ErrorHandlerMiddleware.php
+++ php_back-end_sviluppo/Middleware/ErrorHandlerMiddleware.php
@@ -7,15 +7,28 @@ use Psr\Http\Server\RequestHandlerInterface as Handler;
 use Util\StatusCode;
 use Util\JsonResponse;
 use Slim\Psr7\Response as SlimResponse;
+use Psr\Log\LoggerInterface;
 
 class ErrorHandlerMiddleware
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
     public function __invoke(Request $request, Handler $handler): Response
     {
         try {
             return $handler->handle($request);
         } catch (\Throwable $e) {
-            error_log('[ErrorHandler] ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine());
+            $this->logger->error('Eccezione non gestita', [
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'route' => $request->getUri()->getPath(),
+                'method' => $request->getMethod(),
+            ]);
             return JsonResponse::error(new SlimResponse(), StatusCode::SERVER_ERRORE_INTERNO);
         }
     }
--- php_back-end_sviluppo/conf/config.php
+++ php_back-end_sviluppo/conf/config.php
@@ -6,4 +6,6 @@ return [
     'BASEPATH'   => '/v1',
     'JWT_SECRET' => '5ba298f2fd2bba364372ce93a789edd79f90251ddd136dc12bf8fae6948aa6b9',
     'JWT_TTL'    => 86400, // in secondi —> 86400 = 24 ore
+    'LOKI_URL' => 'http://host.docker.internal:3100',
+    'APP_NAME' => 'Navipark',
 ];
